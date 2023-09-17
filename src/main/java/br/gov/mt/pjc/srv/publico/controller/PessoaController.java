59e8f61deaba5673557f5329bfeb36e1d42c8224
Valdeir Nascimento <valdeircn11@gmail.com>
2023-09-16 21:44:48 -0300

    Criação de endpoint para listar pessoas.

---
+++ src/main/java/br/gov/mt/pjc/srv/publico/controller/PessoaController.java
@@ -0,0 +1,28 @@
+package br.gov.mt.pjc.srv.publico.controller;
+
+import br.gov.mt.pjc.srv.publico.dto.PessoaRequest;
+import br.gov.mt.pjc.srv.publico.dto.PessoaResponse;
+import br.gov.mt.pjc.srv.publico.entity.Pessoa;
+import br.gov.mt.pjc.srv.publico.service.IBaseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/api/v1/pessoas")
+public class PessoaController {
+
+    private final IBaseService<Pessoa, PessoaRequest, PessoaResponse> pessoaService;
+
+    @GetMapping
+    public ResponseEntity<List<PessoaResponse>> listar() {
+        List<PessoaResponse> pessoas = pessoaService.listarTodos();
+        return ResponseEntity.ok().body(pessoas);
+    }
+
+}
