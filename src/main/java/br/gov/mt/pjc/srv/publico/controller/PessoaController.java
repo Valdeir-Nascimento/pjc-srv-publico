ff2b19d64aad4300300fdc0422aea40ae5528a19
Valdeir Nascimento <valdeircn11@gmail.com>
2023-09-17 20:18:29 -0300

    Implementação do endpoint para buscar pessoa por id

--- src/main/java/br/gov/mt/pjc/srv/publico/controller/PessoaController.java
+++ src/main/java/br/gov/mt/pjc/srv/publico/controller/PessoaController.java
@@ -7,6 +7,7 @@ import br.gov.mt.pjc.srv.publico.service.IBaseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,12 @@ public class PessoaController {
     public ResponseEntity<List<PessoaResponse>> listar() {
         List<PessoaResponse> pessoas = pessoaService.listarTodos();
         return ResponseEntity.ok().body(pessoas);
+    }
+
+    @GetMapping("/{idPessoa}")
+    public ResponseEntity<PessoaResponse> buscar(@PathVariable Integer idPessoa) {
+        PessoaResponse pessoaResponse = pessoaService.buscarPorId(idPessoa);
+        return ResponseEntity.ok().body(pessoaResponse);
     }
 
 }
